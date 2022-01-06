267d5dc3314eea7a649df4d850bad37bf2c58076
Ka Delanegri <k.delanegri@gmail.com>
2022-01-05 21:43:37 -0300

    switch atualizado com defaults indefinidos e com erros

--- JAVA Tests/First Activities/EstruturasSwitchAndIf.java
+++ JAVA Tests/First Activities/EstruturasSwitchAndIf.java
@@ -11,7 +11,7 @@ public class EstruturasSwitchAndIf {
     }
 
     private static void ifFlecha() { //exemplo de if em flecha, código ilegível
-        int mes = 4; //entrada do número do mês equivalente
+        int mes = 14; //entrada do número do mês equivalente
         if (mes == 1) {
             System.out.println("Janeiro");
         } else {
@@ -29,6 +29,32 @@ public class EstruturasSwitchAndIf {
                         } else {
                             if (mes == 6) {
                                 System.out.println("Junho");
+                            } else {
+                                if (mes == 7) {
+                                    System.out.println("Julho");
+                                } else {
+                                    if (mes == 8) {
+                                        System.out.println("Agosto");
+                                    } else {
+                                        if (mes == 9) {
+                                            System.out.println("Setembro");
+                                        } else {
+                                            if (mes == 10) {
+                                                System.out.println("Outubro");
+                                            } else {
+                                                if (mes == 11) {
+                                                    System.out.println("Novembro");
+                                                } else {
+                                                    if (mes == 12) {
+                                                        System.out.println("Dezembro");
+                                                    } else {
+                                                        System.out.println("Mês Indefinido");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
@@ -38,7 +64,7 @@ public class EstruturasSwitchAndIf {
     } //estrutura de flecha >
 
     private static void ifSemFlecha() { //utilização do if else if, maior legibilidade ao código
-        int mesSemFlecha = 3;
+        int mesSemFlecha = 13;
         if (mesSemFlecha == 1) {
             System.out.println("Janeiro");
         }
@@ -47,11 +73,13 @@ public class EstruturasSwitchAndIf {
         }
         else if (mesSemFlecha == 3) {
             System.out.println("Março");
+        } else {
+            System.out.println("Mês Indefinido");
         }
     }
 
     private static void switchSemana() { //estrutura switch para um único valor (no caso String que devolve o número do mês equivalente)
-        String diaDaSemana = "Segunda";
+        String diaDaSemana = "karine";
         switch (diaDaSemana) {
             case "Segunda":
                 System.out.println(2);
@@ -62,11 +90,13 @@ public class EstruturasSwitchAndIf {
             case"Quarta":
                 System.out.println(4);
                 break;
+            default:
+                System.out.println("Por favor, coloque um dia válido");
         }
     }
 
     private static void switchFerias() { //switch para um conjunto de Strings (no caso: Janeiro, Julho e Dezembro) retornando uma mensagem
-        String mesesFerias = "Janeiro";
+        String mesesFerias = "karine";
         switch (mesesFerias) {
             case "Janeiro":
             case "Julho":
@@ -74,8 +104,9 @@ public class EstruturasSwitchAndIf {
                 System.out.println("Férias!!!");
                 break;
             default:
-                System.out.println("Trabalhando");
+                System.out.println("Mês Indefinido"); //o default é usado apenas para apontar que a informação apontado não é a correta relacionada com o valor, NÃO é para usar como um valor definitivo ou opção de valor
                 break;
+            }
             /*case "Fevereiro":
             case "Março":
             case "Abril":
@@ -85,12 +116,7 @@ public class EstruturasSwitchAndIf {
             case "Setembro":
             case "Outubro":
             case "Novembro":
-                System.out.println("Trabalhando");
+                System.out.println("Indefinido");
                 break;*/ //parte do exercício que fiz sozinha, coloquei um case para cada opção não desejada, mas pode ser diminuído para o default e depois o break
         }
     }
-}
-
-
-
-
